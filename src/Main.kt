3603803d0705219e934ce92a9c01d3064232682e
Brandon Wallace <brandon@experoinc.com>
2024-12-03 22:47:33 -0600

    day 3 part 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,21 +1,49 @@
 import java.io.File
 
-data class BinaryOperator(val operator: String, val operands: Pair<Int, Int>)
+interface Instruction
+data class NullaryOperator(val operator: String): Instruction
+data class BinaryOperator(val operator: String, val operands: Pair<Int, Int>): Instruction
 
-fun findValidInstructions(data: String): Sequence<BinaryOperator> {
-  val regex = """(?<operator>mul)\((?<operand1>\d{1,3}),(?<operand2>\d{1,3})\)""".toRegex()
+fun findValidInstructions(data: String): Sequence<Instruction> {
+  val regex = """(?<operator>mul|don't|do)\((?<operands>\d{1,3}(?:,\d{1,3})*)?\)""".toRegex()
 
   // find all matches and yield BinaryOperator for each match
   return regex.findAll(data).map {
-    BinaryOperator(it.groups["operator"]!!.value, Pair(it.groups["operand1"]!!.value.toInt(), it.groups["operand2"]!!.value.toInt()))
+    val operands = it.groups["operands"]?.value?.split(",") ?: listOf()
+    val operator = it.groups["operator"]!!.value
+    when (operands.size) {
+      0 -> when (operator) {
+        "do", "don't" -> NullaryOperator(operator)
+        else -> null
+      }
+      2 -> when (operator) {
+        "mul" -> BinaryOperator(operator, operands.map { it.toInt()}.zipWithNext().single())
+        else -> null
+      }
+      else -> null
+    }
   }
+    .filterNotNull()
 }
 
-fun executeProgram(instructions: Sequence<BinaryOperator>): Int {
+fun executeProgram(instructions: Sequence<Instruction>): Int {
   var accumulator = 0
+  var enabled = true
   for (instruction in instructions) {
-    when (instruction.operator) {
-      "mul" -> accumulator += instruction.operands.first * instruction.operands.second
+    when (instruction) {
+      is NullaryOperator -> {
+        when (instruction.operator) {
+          "do" -> enabled = true
+          "don't" -> enabled = false
+        }
+      }
+      is BinaryOperator -> {
+        if (enabled) {
+          when (instruction.operator) {
+            "mul" -> accumulator += instruction.operands.first * instruction.operands.second
+          }
+        }
+      }
     }
   }
   return accumulator
