a1f25ea64681d7003bfc5e7b904987cf24489ff2
Brandon Wallace <brandon.wallace@yahoo.com>
2024-12-06 23:58:29 -0600

    day 7 part 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,94 +1,54 @@
 import java.io.File
 
-data class Coord(val x: Int, val y: Int) {
-  operator fun plus(other: Coord): Coord {
-    return Coord(x + other.x, y + other.y)
-  }
+data class Equation(val testValue: Long, val operands: List<Long>)
 
-  fun rotate90(): Coord {
-    return Coord(-y, x)
-  }
-}
-data class GuardPos(val loc: Coord, val dir: Coord) {
-  constructor(loc: Coord, indicator: String) :
-    this(
-      loc,
-      Coord(
-        when (indicator) {
-          "^", "v" -> 0
-          "<" -> -1
-          else -> 1
-        }, when (indicator) {
-          "<", ">" -> 0
-          "^" -> -1
-          else -> 1
-        }
-      )
-    )
-}
+val operators = listOf("+", "*")
 
-data class LabMap(val lines: List<String>) {
-  val W: Int get() = lines[0].length
-  val H: Int get() = lines.size
-
-  fun get(c: Coord): Char {
-    return lines[c.y][c.x]
-  }
-
-  fun isOutside(c: Coord): Boolean {
-    return c.x < 0 || c.x >= W || c.y < 0 || c.y >= H
-  }
-
-  fun findGuard(): GuardPos {
-    lines.forEachIndexed { y, line ->
-      run {
-        val r = line.findAnyOf(listOf("<", ">", "v", "^"))
-        if (r != null) {
-          return GuardPos(Coord(r.first, y), r.second)
+fun processSubEquation(target: Long, subEquation: List<Long>): List<Long> {
+  when (subEquation.size) {
+    1 -> return subEquation
+    2 -> {
+      return operators.map { operator ->
+        when (operator) {
+          "+" -> subEquation[0] + subEquation[1]
+          "*" -> subEquation[0] * subEquation[1]
+          else -> 0
         }
       }
     }
-    throw Exception("No guards found")
+    else -> {
+      // process children and filter out the ones that are past our target already
+      val currentNumber = subEquation.last()
+      val children = processSubEquation(target, subEquation.subList(0, subEquation.size - 1)).filter { it <= target }
+      return operators.flatMap { operator ->
+        when (operator) {
+          "+" -> children.map { currentNumber + it }
+          "*" -> children.map { currentNumber * it }
+          else -> listOf(0)
+        }}
+    }
   }
 }
 
-fun tryObstaclePosition(map: LabMap, obstacle: Coord): Boolean {
-  var guard = map.findGuard()
-  val visited = mutableSetOf(guard)
-
-  while (!map.isOutside(guard.loc + guard.dir)) {
-    while (map.get(guard.loc + guard.dir) == '#' || obstacle == guard.loc + guard.dir) {
-      guard = guard.copy(dir = guard.dir.rotate90())
-    }
-    guard = guard.copy(loc = guard.loc + guard.dir)
-    if (visited.contains(guard)) {
-      return true
-    }
-    visited.add(guard)
-  }
-
-  return false
+fun processEquation(equation: Equation): Boolean {
+  return processSubEquation(equation.testValue, equation.operands).any { it == equation.testValue }
 }
 
 fun main() {
   // Read the input file
-  val input = File("data/input06.txt").readLines()
-  val map = LabMap(input)
-  var guard = map.findGuard()
-  val invalidLoc = guard.loc
-  val possibleLocations = mutableSetOf<Coord>()
+  val input = File("data/input07.txt").readLines()
 
-  while (!map.isOutside(guard.loc + guard.dir)) {
-    while (map.get(guard.loc + guard.dir) == '#') {
-      guard = guard.copy(dir = guard.dir.rotate90())
-    }
-    guard = guard.copy(loc = guard.loc + guard.dir)
-    if (guard.loc != invalidLoc) {
-      possibleLocations.add(guard.loc)
-    }
+  val result = input.map { line ->
+    line.split("[:\\s]+".toRegex())
+      .map { it -> it.toLong() }
+      .let { parts -> Equation(parts[0], parts.subList(1, parts.size)) }
   }
-
-  val result = possibleLocations.filter { tryObstaclePosition(map, it) }.size
+    .filter { equation -> processEquation(equation) }
+    .filter {
+      println("Kept ${it.testValue}")
+      true
+    }
+    .sumOf { it.testValue }
 
   println(result)
 }
